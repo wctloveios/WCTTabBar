068d442e283036d0994c21671369cdbe13c2e3e2
wct <wct@wct-mac.local>
2018-01-24 16:11:17 +0800

    Initial Commit

---
+++ WCTTabBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WCTTabBar
+//
+//  Created by wct on 2018/1/24.
+//  Copyright © 2018年 wct. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WCTTabBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WCTTabBar
+//
+//  Created by wct on 2018/1/24.
+//  Copyright © 2018年 wct. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
